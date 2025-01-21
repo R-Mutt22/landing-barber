01add8fc2092fff7df8170bd8844ed9b62dcbb22
r.mutt22 <matiaszelarayan87@gmail.com>
2025-01-21 18:13:20 -0300

    astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+
+  site: 'https://R-Mutt22.github.io',
+  base: 'landing-barber',
 });
